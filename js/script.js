6059573c25d280fe14f3e172e96e16aeb8bf55d0
Pedro Augusto Cabral Oliveira <104571614+PedroAugusto2004@users.noreply.github.com>
2024-07-08 22:49:09 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,25 +22,3 @@ function calculateResult() {
     }
 }
 
-//----------DARKMODE SWITCH----------//
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.getElementById('theme-toggle');
-
-    // Check user's theme preference on page load
-    if (localStorage.getItem('theme') === 'dark') {
-        document.body.classList.add('dark-mode');
-        toggleButton.classList.add('dark-mode');
-    }
-
-    toggleButton.addEventListener('click', () => {
-        document.body.classList.toggle('dark-mode');
-        toggleButton.classList.toggle('dark-mode');
-
-        // Save the user's preference to localStorage
-        if (document.body.classList.contains('dark-mode')) {
-            localStorage.setItem('theme', 'dark');
-        } else {
-            localStorage.setItem('theme', 'light');
-        }
-    });
-});
